4774f862a2d619bc00a9804ed7d8e60f29d08a59
Melvin0070 <melvin.aspirant@gmail.com>
2023-08-22 09:50:31 +0530

    Completed all challeneges

--- src/app.js
+++ src/app.js
@@ -23,11 +23,21 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+    let detailedPlayers = [];
+    players.map((player, index) => {
+        let data = {
+          name: player,
+          strength: getRandomStrength(),
+          image: `images/super-${index + 1}.png`,
+          type: index % 2 === 0 ? "hero" : "villain",
+        };
+        detailedPlayers.push(data);
+      });
 
     // Instead of forloop use Map method
     // Code here
 
+    
     return detailedPlayers;
 }
 
@@ -43,6 +53,16 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    fragment = players
+    .filter((player) => player.type === type)
+    .map(
+      (player) => `<div class="player">
+        <img src="${player.image}" alt="">
+        <div class="name">${player.name}</div>
+        <div class="strength">${player.strength}</div></div>`
+    )
+    .join("");
+
 
     return fragment;
 }
